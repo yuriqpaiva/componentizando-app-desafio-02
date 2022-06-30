becb654d86c345f7121d2b5536231bb376968efa
Yuri <yuriqpaiva@gmail.com>
2022-06-30 15:06:45 -0300

    Add sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: {
+    id: number;
+    title: string;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  }[];
+  selectedGenreId: number;
+  handleGenreChange: (id: number) => void;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleGenreChange,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleGenreChange(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
